b9a21de914f17cda70ee134064d1c3e5677a0f17
Oxidiot <yablinchik@gmail.com>
2020-11-26 16:34:20 +0500

    Fix data type in task 4

--- lab3/task4.c
+++ lab3/task4.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int* gen_array(unsigned int len,int range_start,int range_end){
+char* gen_array(unsigned int len,char range_start,char range_end){
     
-    int* array = malloc(len*sizeof(int));
+    char* array = malloc(len*sizeof(char));
     for(int i=0;i<len;i++){
         array[i]=rand()%(range_end - range_start + 1) + range_start;
 
     }
     return array;
 }
-void insertion_sort_desc(int* array,unsigned int len){
+void insertion_sort_desc(char* array,unsigned int len){
     for(int i=1;i<len;i++){
-        int k = array[i];
+        char k = array[i];
         int j = i;
         while(--j>=0 && array[j]<k){
             array[j+1]=array[j];
@@ -21,24 +21,26 @@ void insertion_sort_desc(int* array,unsigned int len){
     }
 }
 int main(){
-    unsigned int len;
+    unsigned int len; 
+    char range_start,range_end;
+    printf("Range: ");
+    scanf("%c %c",&range_start,&range_end);
     printf("Array length: ");
     scanf("%u",&len);
-    int range_start,range_end;
-    printf("Start: ");
-    scanf("%d",&range_start);
-    printf("End: ");
-    scanf("%d",&range_end);
+  
+    
+
+
     srand(time(0));
-    int* array = gen_array(len,range_start,range_end);
+    char* array = gen_array(len,range_start,range_end);
     printf("Generated array: ");
     for(int i=0;i<len;i++)
-        printf("%d ",array[i]);
+        printf("%c ",array[i]);
     printf("\b\r\n");
     insertion_sort_desc(array,len);
     printf("Sorted array: ");
     for(int i=0;i<len;i++)
-        printf("%d ",array[i]);
+        printf("%c ",array[i]);
     printf("\b\r\n");
     return 0;
     
